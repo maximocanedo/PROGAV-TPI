b3d56788255c82871c15e9cd6063a30b183a4aac
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2024-07-03 16:17:40 -0300

    Inicializar proyecto

---
+++ src/main/java/utn/frgp/prog/TpiApplication.java
@@ -0,0 +1,13 @@
+package utn.frgp.prog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/utn/frgp/prog/TpiApplicationTests.java
@@ -0,0 +1,13 @@
+package utn.frgp.prog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
